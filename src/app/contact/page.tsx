5fd29d450b9e8faaf4e002d3aab2bcbcb341849e
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-05-10 10:31:31 -0500

    finishing contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import styles from './contact.module.css';
 
 const ContactPage = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image 
+          src='/contact.png' 
+          alt='contact' fill
+          className={styles.logo} 
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <h2 className={styles.title}>Contact Us</h2>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name and Surename *"/>
+          <input type="text" placeholder="Email Address *"/>
+          <input type="text" placeholder="Phone Number (optional)"/>
+          <textarea 
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder='Message'
+          >
+          </textarea>
+          <button className={styles.send}>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
