b2bbfcc23c68cd00b1bd49e99431118edd02a3e4
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-05-10 10:37:41 -0500

    adding styles in body (tag)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='container'>
         <Navbar />
         {children}
         <Footer />
