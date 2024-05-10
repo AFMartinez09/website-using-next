5e0f2a1e1f180fa6ef1b5a3141f32ddc4d41903b
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-05-10 10:31:11 -0500

    finishing about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,41 @@
+import Image from "next/image"
+import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.title}>About Ground</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Harum sequi commodi esse labore eaque at rem ex molestias iure, 
+          eos, illum veniam odio ullam maiores suscipit. Incidunt ipsum neque veniam!
+        </p>
+        <br/>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Harum sequi commodi esse labore eaque at rem ex molestias iure, 
+          eos, illum veniam odio ullam maiores suscipit. Incidunt ipsum neque veniam!
+        </p>
+        <br/>
+        <br/>
+        <br/>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Harum sequi commodi esse labore eaque at rem ex molestias iure, 
+          eos, illum veniam odio ullam maiores suscipit. Incidunt ipsum neque veniam!
+        </p>
+      </div>
+      <div className={styles.imageContainer}>
+      <Image src='/aboutUs.png'  alt="" fill/>
+      </div>
+    </div>
   )
 }
 
 export default AboutPage
+
+
+
+
+
